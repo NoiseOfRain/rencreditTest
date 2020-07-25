d3171b019eed38ccb50b672360c997befe87f094
Стародубцев Виталий Сергеевич <v.starodubtsev@bssys.com>
2020-07-25 12:23:13 +0300

    start

---
+++ src/test/java/secondTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class secondTest {
+
+    @Test
+    public void logic() {
+        System.out.println("ololo");
+//        open("https://rencredit.ru/");
+//        $("a[href=\"/contributions/\"]").click();
+    }
+
+}
